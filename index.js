e47ba75bdb86afd3aa8ce509f1248a9ed4c1267a
Harsh Kumar <[harshkr.221104@gmail.com]>
2026-07-08 14:58:24 +0530

    edit index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 5000;
 const app=express();
 
 app.get('/', (req, res) => {
-    res.send('AWS Deployment Learning Process on EC2 Instance');
+    res.send('AWS Deployment Learning Process on EC2 Instance by changing the code and pushing it to GitHub and then pulling it from EC2 Instance');
 });
 app.get('/about', (req, res) => {
-    res.send('About Page of the AWS Learning Process');
+    res.send('About Page of the AWS Learning Process and changes made in the code and pushed to GitHub and then pulled from EC2 Instance');
 });
 
 app.listen(PORT, () => {
